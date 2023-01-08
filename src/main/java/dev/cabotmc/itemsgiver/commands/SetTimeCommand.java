3a6a3be17b460452a9ba9f47c36d119fa561750b
Holly <laurenhollymunro@gmail.com>
2023-01-07 22:57:30 -0400

    added stuff

--- src/main/java/dev/cabotmc/itemsgiver/commands/SetTimeCommand.java
+++ src/main/java/dev/cabotmc/itemsgiver/commands/SetTimeCommand.java
@@ -1,9 +1,13 @@
 package dev.cabotmc.itemsgiver.commands;
 
+import dev.cabotmc.itemsgiver.giver.BossBarManager;
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.arguments.IntegerArgument;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public class SetTimeCommand {
 
@@ -11,9 +15,9 @@ public class SetTimeCommand {
         if (!(sender instanceof Player)) return;
         var player = (Player) sender;
         int time = (Integer) args[0];
-
-
+        BossBarManager.getPrefs(player).setMaxTime(time);
     }
+    public static HashMap<UUID,Long> lastruntimes = new HashMap<>();
 
     public static void register() {
         new CommandAPICommand("itemdelay")
