cb2682ff201427e1024e98546ac186e9a1e3673c
Kyle Smith <kyleafmine@gmail.com>
2023-01-08 03:45:35 -0400

    changes

--- src/main/java/dev/cabotmc/itemsgiver/GamesManager.java
+++ src/main/java/dev/cabotmc/itemsgiver/GamesManager.java
@@ -2,6 +2,10 @@ package dev.cabotmc.itemsgiver;
 
 import com.destroystokyo.paper.event.server.ServerTickStartEvent;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.Style;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.title.Title;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
@@ -9,7 +13,9 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -68,13 +74,39 @@ public class GamesManager implements Listener {
 
         // make 5x5 bedrock platform
         for (int x = -2; x < 3; x++) {
-                for (int z = -2; z < 3; z++) {
-                    mainWorld.setBlockData(x, 64, z, Material.BEDROCK.createBlockData());
-                }
+            for (int z = -2; z < 3; z++) {
+                mainWorld.setBlockData(x, 64, z, Material.BEDROCK.createBlockData());
+            }
         }
 
     }
+    @EventHandler
+    public void death(PlayerDeathEvent e) {
+        e.setCancelled(true);
+        for (var i: e.getPlayer().getInventory()) {
+            e.getPlayer().getWorld().dropItemNaturally(e.getPlayer().getLocation(), i);
+        }
+        for (var i: e.getPlayer().getInventory().getArmorContents()) {
+            e.getPlayer().getWorld().dropItemNaturally(e.getPlayer().getLocation(), i);
+        }
+        e.getPlayer().getInventory().clear();
+        e.getPlayer().getInventory().setArmorContents(new ItemStack[4]);
+        e.getPlayer().getWorld().spigot().strikeLightningEffect(e.getPlayer().getLocation(), false);
+        e.getPlayer().setGameMode(GameMode.SPECTATOR);
+        if (e.getPlayer().getLocation().getY() < 0) {
+            e.getPlayer().teleport(e.getPlayer().getLocation().set(0, 64, 0));
+        }
+        e.getPlayer().showTitle(Title.title(
+                Component.text("You Died", Style.style(style -> {
+                    style.color(TextColor.color(0xeb4034));
+                    style.decorate(TextDecoration.BOLD);
+                })),
+                Component.text("")
+        ));
+        e.getPlayer().setTotalExperience(0);
+        Bukkit.getServer().sendMessage(e.deathMessage());
 
+    }
     public void enableSpectating(Player p) {
         p.getPersistentDataContainer().set(SPECTATOR_KEY, PersistentDataType.BYTE, (byte) 1);
         for (Player t : Bukkit.getOnlinePlayers()) {
@@ -86,7 +118,7 @@ public class GamesManager implements Listener {
 
     }
     static boolean isSpectator(Player p ) {
-        return p.getPersistentDataContainer().has(SPECTATOR_KEY);
+        return p.getGameMode() == GameMode.SPECTATOR || p.getPersistentDataContainer().has(SPECTATOR_KEY);
     }
 
 }
